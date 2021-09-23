83732e3b68b1ac7a6199501b87de09986932535d
Maks <makskakzey@ukr.net>
2021-09-23 13:01:31 +0300

    add new functional on main files

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', Home.as_view(), name='home'),
+    path('category/', get_category, name='category'),
+    path('category/<str:title>/', PostByCategory.as_view(), name='one_category'),
+    path('post/<str:slug>/', get_post, name='post'),
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
+from .models import *
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Photo Blog'
+        return context
+
+
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__title=self.kwargs['title'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(title=self.kwargs['title'])
+        return context
 
 
 def index(request):
     return render(request, 'blog/index.html')
+
+
+def get_category(request):
+    return render(request, 'blog/category.html')
+
+
+def get_post(request, slug):
+    return render(request, 'blog/index.html')
+
